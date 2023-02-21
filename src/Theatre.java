51f38db655e431773115d440674df8a811a7f2c2
athuk <irushaathukorala@gmail.com>
2023-02-21 11:00:17 +0530

    test commit

---
+++ src/Theatre.java
@@ -0,0 +1,216 @@
+import java.util.Scanner;
+
+public class Theatre {
+    static int[] row1;
+    static int[] row2;
+    static int[] row3;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the New Theatre");
+        row1 = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        row2 = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        row3 = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
+        boolean flag = true;
+        while (flag) {
+            System.out.println("""
+                    -------------------------------------------------
+                    Please select an option:\s
+                    1) Buy a ticket\s
+                    2) Print seating area\s
+                    3) Cancel ticket\s
+                    4) List available seats\s
+                    5) Save to file\s
+                    6) Load from file\s
+                    7) Print ticket information and total price\s
+                    8) Sort tickets by price\s
+                     0) Quit\s
+                    -------------------------------------------------
+                    """);
+            switch (getPositiveInt("Enter option:", true)) {
+                case 1:
+                    buy_ticket();
+                    break;
+                case 0:
+                    flag = false;
+                    break;
+                case 2:
+                    print_seating_area();
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private static void print_seating_area() {
+        System.out.println("""
+                     ***********\s
+                     *  STAGE  *\s
+                     ***********\s
+                """);
+
+        System.out.print("    ");
+        for (int i = 0; i < row1.length; i++) {
+            if (i == row1.length/2) {
+                System.out.print(" ");
+            }
+            printSeat(row1[i]);
+        }
+        System.out.println();
+        System.out.print("  ");
+        for (int i = 0; i < row2.length; i++) {
+            if (i == row2.length/2) {
+                System.out.print(" ");
+            }
+            printSeat(row2[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < row3.length; i++) {
+            if (i == row3.length/2) {
+                System.out.print(" ");
+            }
+            printSeat(row3[i]);
+        }
+        System.out.println();
+    }
+
+    static void printSeat(int seatVal) {
+        if (seatVal == 0) {
+            System.out.print("O");
+        } else {
+            System.out.print("X");
+        }
+    }
+
+    static int getPositiveInt(String message, String tryAgainMessage) {
+        System.out.println(message);
+        Scanner sc = new Scanner(System.in);
+
+        String strInt;
+
+        do {
+            strInt = sc.nextLine();
+            try {
+                int parsedInt = Integer.parseInt(strInt);
+                if (parsedInt > 0) {
+                    return parsedInt;
+                } else {
+                    System.out.println(tryAgainMessage);
+                }
+            } catch (Exception e) {
+                System.out.println(tryAgainMessage);
+            }
+        }
+
+        while (true);
+    }
+
+    static void cancel_ticket() {
+        int seatRow = getSeatRow();
+        int seat = getSeat(seatRow, false);
+    }
+
+    static int getPositiveInt(String message, boolean includesZero) {
+        Scanner sc = new Scanner(System.in);
+
+        String strInt;
+
+        do {
+            System.out.println(message);
+            strInt = sc.nextLine();
+            try {
+                int parsedInt = Integer.parseInt(strInt);
+                if (includesZero) {
+                    if (parsedInt >= 0) {
+                        return parsedInt;
+                    } else {
+                        System.out.println();
+                    }
+                } else {
+                    if (parsedInt > 0) {
+                        return parsedInt;
+                    } else {
+                        System.out.println();
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println();
+            }
+        }
+
+        while (true);
+    }
+
+    static int getSeatRow() {
+
+        while (true) {
+            int tempSeat = getPositiveInt("Please Enter the seat row", "Please enter a positive integer");
+
+            switch (tempSeat) {
+                case 1:
+                case 2:
+                case 3:
+                    return tempSeat;
+                default:
+                    System.out.println("Invalid seat row. Please try again.");
+                    break;
+            }
+        }
+
+    }
+
+    static int getSeat(int selectedSeatRow, boolean checkAlreadyBooked) {
+
+        while (true) {
+            int tempSeat = getPositiveInt("Please Enter the seat", "Please enter a positive integer");
+
+            switch (selectedSeatRow) {
+                case 1:
+                    if (tempSeat > 12) {
+                        System.out.println("Enter a seat number between 1 - 12");
+                    } else {
+                        if (checkAlreadyBooked){
+
+                        }
+                        if (row1[tempSeat - 1] == 0) {
+                            return tempSeat;
+                        } else {
+                            System.out.println("Seat already booked");
+                        }
+                    }
+                    break;
+                case 2:
+                    if (tempSeat > 16) {
+                        System.out.println("Enter a seat number between 1 - 16");
+                    } else {
+                        if (row2[tempSeat - 1] == 0) {
+                            return tempSeat;
+                        } else {
+                            System.out.println("Seat already booked");
+                        }
+                    }
+                    break;
+                case 3:
+                    if (tempSeat > 20) {
+                        System.out.println("Enter a seat number between 1 - 20");
+                    } else {
+                        if (row3[tempSeat - 1] == 0) {
+                            return tempSeat;
+                        } else {
+                            System.out.println("Seat already booked");
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+    static void buy_ticket() {
+        int selectedSeatRow = getSeatRow();
+        int selectedSeat = getSeat(selectedSeatRow, false);
+
+        System.out.printf("Booked seat %s from row 1\n", selectedSeat);
+        row1[selectedSeat - 1] = 1;
+    }
+}
