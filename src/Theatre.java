b0ca93f68c96c1c0eed607029ff36ed35af8e4b6
athuk <irushaathukorala@gmail.com>
2023-03-18 21:33:50 +0530

    final touches

--- src/Theatre.java
+++ src/Theatre.java
@@ -8,7 +8,6 @@ public class Theatre {
     static int[] row3;
 
     static ArrayList<Ticket> tickets = new ArrayList<>();
-
     static boolean BOOKING_MODE = true;
     static boolean CANCELLING_MODE = false;
 
@@ -36,29 +35,16 @@ public class Theatre {
                     -------------------------------------------------
                     """);
             switch (getPositiveInt("Enter option:", true)) {
-                case 1:
-                    buy_ticket();
-                    break;
-                case 0:
-                    flag = false;
-                    break;
-                case 2:
-                    print_seating_area();
-                    break;
-                case 3:
-                    cancel_ticket();
-                    break;
-                case 4:
-                    available_seats();
-                    break;
-                case 5:
-                    save();
-                    break;
-                case 6:
-                    load();
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
+                case 1 -> buy_ticket();
+                case 0 -> flag = false;
+                case 2 -> print_seating_area();
+                case 3 -> cancel_ticket();
+                case 4 -> available_seats();
+                case 5 -> save();
+                case 6 -> load();
+                case 7 -> show_tickets_info();
+                case 8 -> sort_tickets();
+                default -> System.out.println("Invalid option. Please try again.");
             }
         }
     }
@@ -126,6 +112,34 @@ public class Theatre {
         while (true);
     }
 
+    /**
+     *
+     * @param message
+     * @param tryAgainMessage
+     * @return
+     */
+    static float getPositiveFloat(String message, String tryAgainMessage) {
+        System.out.println(message);
+        Scanner sc = new Scanner(System.in);
+
+        String strFloat;
+
+        do {
+            strFloat = sc.nextLine();
+            try {
+                float parsedInt = Float.parseFloat(strFloat);
+                if (parsedInt > 0) {
+                    return parsedInt;
+                } else {
+                    System.out.println(tryAgainMessage);
+                }
+            } catch (Exception e) {
+                System.out.println(tryAgainMessage);
+            }
+        }
+
+        while (true);
+    }
     static void available_seats() {
         System.out.print("Seats available in row 1: ");
 
@@ -156,6 +170,11 @@ public class Theatre {
         System.out.println(arrayToString(tempArray) + ".");
     }
 
+    /**
+     * will take an ArrayList of type integer
+     * @param list is the ArrayList
+     * @return a formatted string with the array elements
+     */
     static String arrayToString(ArrayList<Integer> list) {
         StringBuilder retStr = new StringBuilder();
         for (int i = 0; i < list.size(); i++) {
@@ -183,30 +202,38 @@ public class Theatre {
         int seat = getSeat(seatRow, CANCELLING_MODE);
         int[] rowArray;
         if (seat != -1) {
-            switch (seatRow) {
-                case 1:
-                    rowArray = row1;
-                    break;
-                case 2:
-                    rowArray = row2;
-                    break;
-                case 3:
-                    rowArray = row3;
-                    break;
-                default:
-                    rowArray = new int[0];
-                    break;
-            }
+            rowArray = switch (seatRow) {
+                case 1 -> row1;
+                case 2 -> row2;
+                case 3 -> row3;
+                default -> new int[0];
+            };
             rowArray[seat - 1] = 0;
-            tickets.remove(getTicketIndex(seatRow, seat, tickets));
+            int ticketIndex = getTicketIndex(seatRow, seat, tickets);
+            if (ticketIndex != -1) {
+                tickets.remove(ticketIndex);
+            }
             System.out.printf("Cancelled ticket %s from row %s\n", seat, seatRow);
             //System.out.println(tickets.size());
         }
     }
 
+    static void sort_tickets() {
+        for (int i = 0; i < tickets.size(); i++) {
+            for (int j = i + 1; j < tickets.size(); j++) {
+                if (tickets.get(i).getPrice() > tickets.get(j).getPrice()) {
+                    Ticket tempTicket = tickets.get(i);
+                    tickets.set(i, tickets.get(j));
+                    tickets.set(j, tempTicket);
+                }
+            }
+        }
+        show_tickets_info();
+    }
+
     static int getTicketIndex(int row, int seat, ArrayList<Ticket> ticketArray) {
         for (int i = 0; i < ticketArray.size(); i++) {
-            if (ticketArray.get(i).row == row && ticketArray.get(i).seat == seat) {
+            if (ticketArray.get(i).getRow() == row && ticketArray.get(i).getSeat() == seat) {
                 return i;
             }
         }
@@ -214,22 +241,52 @@ public class Theatre {
         return -1;
     }
 
+    static String emailInput() {
+        Scanner sc = new Scanner(System.in);
+        String email = "";
+        do {
+            if (!email.equals("") && !emailFormatCheck(email)) {
+                System.out.println("Email is in wrong format. Please try again.");
+            }
+            email = sc.nextLine();
+        }
+        while (!emailFormatCheck(email));
+        return email;
+    }
+
+    static boolean emailFormatCheck(String email) {
+        if (email.contains("@")) {
+            try {
+                if (email.split("@")[1].split("\\.").length == 2) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                return false;
+            }
+        }
+
+        else {
+            return false;
+        }
+    }
     static void save() {
         File file = new File("src\\data.txt");
 
         try {
             if (file.createNewFile()) {
                 System.out.println("File created");
-            }
-
-            else {
+            } else {
                 System.out.println("File overwritten");
             }
         } catch (IOException e) {
             System.out.println("Unable to create a file");
         }
 
-        try{
+        try {
             FileWriter writer = new FileWriter(file);
             writer.write(String.format("Row 1 - %s\n", arrayToString(row1)));
             writer.write(String.format("Row 2 - %s\n", arrayToString(row2)));
@@ -237,8 +294,7 @@ public class Theatre {
             writer.close();
             System.out.println("Successfully wrote data to the file");
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Created file isn't available.");
             throw new RuntimeException(e);
         }
@@ -253,18 +309,15 @@ public class Theatre {
         return tempArray;
     }
 
-    static boolean rowLengthCheck(int[] array,  int row) {
-        switch (row) {
-            case 1:
-                return array.length == 12;
-            case 2:
-                return array.length == 16;
-            case 3:
-                return array.length == 20;
-            default:
-                return false;
-        }
+    static boolean rowLengthCheck(int[] array, int row) {
+        return switch (row) {
+            case 1 -> array.length == 12;
+            case 2 -> array.length == 16;
+            case 3 -> array.length == 20;
+            default -> false;
+        };
     }
+
     static void load() {
         File arrayFile = new File("src/data.txt");
 
@@ -276,8 +329,7 @@ public class Theatre {
 
             if (rowLengthCheck(rowTemp, 1)) {
                 row1 = rowTemp;
-            }
-            else {
+            } else {
                 throw new FileNotFoundException("Invalid File");
             }
 
@@ -286,8 +338,7 @@ public class Theatre {
 
             if (rowLengthCheck(rowTemp, 2)) {
                 row2 = rowTemp;
-            }
-            else {
+            } else {
                 throw new FileNotFoundException("Invalid File");
             }
 
@@ -296,8 +347,7 @@ public class Theatre {
 
             if (rowLengthCheck(rowTemp, 3)) {
                 row3 = rowTemp;
-            }
-            else {
+            } else {
                 throw new FileNotFoundException("Invalid File");
             }
             System.out.println("Successfully loaded the file");
@@ -339,19 +389,28 @@ public class Theatre {
         while (true);
     }
 
+    static void show_tickets_info() {
+        int i = 0;
+
+        for (Ticket ticket : tickets) {
+            ticket.print();
+            System.out.println();
+            i += ticket.getPrice();
+        }
+
+        System.out.println("Total value of the tickets: " + i);
+    }
+
     static int getSeatRow() {
 
         while (true) {
             int tempSeat = getPositiveInt("Please Enter the seat row", "Please enter a positive integer");
 
             switch (tempSeat) {
-                case 1:
-                case 2:
-                case 3:
+                case 1, 2, 3 -> {
                     return tempSeat;
-                default:
-                    System.out.println("Invalid seat row. Please try again.");
-                    break;
+                }
+                default -> System.out.println("Invalid seat row. Please try again.");
             }
         }
 
@@ -363,7 +422,7 @@ public class Theatre {
             int tempSeat = getPositiveInt("Please Enter the seat", "Please enter a positive integer");
 
             switch (selectedSeatRow) {
-                case 1:
+                case 1 -> {
                     if (tempSeat > 12) {
                         System.out.println("Enter a seat number between 1 - 12");
                     } else {
@@ -382,8 +441,8 @@ public class Theatre {
                             }
                         }
                     }
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     if (tempSeat > 16) {
                         System.out.println("Enter a seat number between 1 - 16");
                     } else {
@@ -402,8 +461,8 @@ public class Theatre {
                             }
                         }
                     }
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     if (tempSeat > 20) {
                         System.out.println("Enter a seat number between 1 - 20");
                     } else {
@@ -422,7 +481,7 @@ public class Theatre {
                             }
                         }
                     }
-                    break;
+                }
             }
         }
     }
@@ -437,27 +496,18 @@ public class Theatre {
         System.out.println("Please enter the surname of the person");
         String surname = sc.nextLine();
         System.out.println("Please enter the email of the person");
-        String email = sc.nextLine();
+        String email = emailInput();
 
         Person person = new Person(personName, surname, email);
 
-        int[] rowArray;
-        switch (selectedSeatRow) {
-            case 1:
-                rowArray = row1;
-                break;
-            case 2:
-                rowArray = row2;
-                break;
-            case 3:
-                rowArray = row3;
-                break;
-            default:
-                rowArray = new int[0];
-                break;
-        }
+        int[] rowArray = switch (selectedSeatRow) {
+            case 1 -> row1;
+            case 2 -> row2;
+            case 3 -> row3;
+            default -> new int[0];
+        };
 
-        int price = getPositiveInt("Please enter the price of the ticket", "Invalid amount");
+        float price = getPositiveFloat("Please enter the price of the ticket", "Invalid amount");
 
         Ticket ticket = new Ticket(selectedSeatRow, selectedSeat, price, person);
 
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,10 +1,22 @@
 public class Ticket {
-    int row;
-    int seat;
-    int price;
-    Person person;
+    private int row;
+    private int seat;
+    private float price;
+    private Person person;
 
-    public Ticket(int row, int seat, int price, Person person) {
+    public int getRow() {
+        return row;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Ticket(int row, int seat, float price, Person person) {
         this.row = row;
         this.seat = seat;
         this.price = price;
@@ -20,6 +32,6 @@ public class Ticket {
                 Seat: %s
                 Price: %s
                 """,
-                person.name, person.surname, person.email, row, seat, price);
+                person.getName(), person.getSurname(), person.getEmail(), row, seat, price);
     }
 }
